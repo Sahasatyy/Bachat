854b33ac5b83a96e74c6b89396ab7b5c64de192f
Sahas Amatya <133555714+Sahasatyy@users.noreply.github.com>
2024-12-18 20:51:57 +0545

    Add project files.

---
+++ Bachat/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Bachat
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
